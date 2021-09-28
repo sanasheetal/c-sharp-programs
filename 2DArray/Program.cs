d240b849a73e78b931f5754b0c2ae890ae83b01a
sanasheetal <76477804+sanasheetal@users.noreply.github.com>
2021-09-28 18:46:47 +0530

    Add files via upload

---
+++ 2DArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2d Array
+            
+            int[,] numGrid=
+            {
+                {1,2},
+                {3,4},
+                {5,6}
+            };
+            Console.WriteLine(numGrid[1,0]);
+            
+        }
+    }
+}
